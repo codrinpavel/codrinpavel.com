63e53fafbc227d2396ce85ebbc3c0dc9c893629b
Codrin Pavel <zerospree@yahoo.com>
2026-06-01 15:40:00 +0100

    Updates

---
+++ src/data/studies.js
@@ -0,0 +1,73 @@
+export default function () {
+  return [
+    {
+      client: "Dominion Energy",
+      industry: "Sustainability & Corporate Reporting",
+      logo: "dominion-energy.svg",
+      description:
+        "Led frontend development for multiple award-winning CSR, ESG, and DEI reporting platforms for Dominion Energy, delivering responsive, WCAG-compliant frontend experiences optimized for speed and reliability.",
+      focus: [
+        "Accessibility",
+        "WCAG",
+        "Lighthouse",
+        "Core Web Vitals",
+        "Responsive Design"
+      ],
+      development: [
+        "11ty",
+        "Node.js",
+        "JavaScript",
+        "SCSS",
+        "Nunjucks"
+      ],
+    },
+    {
+      client: "National Resident Matching Program (NRMP)",
+      industry: "Healthcare & Graduate Medical Education (GME)",
+      logo: "nrmp.svg",
+      description:
+        "Developed the frontend experience for NRMP.org as part of a broader redesign initiative, delivering a responsive, accessible, and performance-focused WordPress platform supporting complex educational resources and organizational communications.",
+      focus: [
+        "Accessibility",
+        "WCAG",
+        "WordPress Development",
+        "Core Web Vitals",
+        "Responsive Design"
+      ],
+      development: [
+        "WordPress",
+        "PHP",
+        "JavaScript",
+        "SCSS",
+        "ACF",
+        "Gutenberg"
+      ],
+    },
+    {
+      client: "Axios",
+      industry: "Digital Publishing & Media",
+      logo: "axios.svg",
+      description:
+        "Led frontend development and launch for Axios.com, delivering high-performance editorial publishing experiences optimized for accessibility, Core Web Vitals, SEO, and scalable CMS-driven content delivery, supporting millions of monthly readers.",
+      focus: [
+        "Core Web Vitals",
+        "Accessibility",
+        "SEO",
+        "Responsive Design",
+        "CMS-Driven Development",
+        "Social Platform Integrations",
+        "Ad Tech Integrations",
+        "A/B Testing"
+      ],
+      development: [
+        "RebelMouse CMS",
+        "Jinja2",
+        "Mustache",
+        "JavaScript",
+        "SCSS",
+        "Google Analytics",
+        "Google Tag Manager"
+      ],
+    }
+  ]
+};
