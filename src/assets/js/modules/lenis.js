935cfe90c525b4ed2913407a3576c63486a67c6b
Codrin Pavel <zerospree@yahoo.com>
2026-06-01 14:29:22 +0100

    Updates

--- src/assets/js/modules/lenis.js
+++ src/assets/js/modules/lenis.js
@@ -9,4 +9,29 @@ const lenis = new Lenis({
   stopInertiaOnNavigate: true,
 });
 
-export default lenis;
+const totalItems = 4;
+
+function scrollToHorizontalItem(index) {
+  const container = document.querySelector(".hero");
+  const totalItems = 4;
+
+  const scrollStart = container.offsetTop
+  const scrollEnd = container.offsetTop + container.offsetHeight - window.innerHeight;
+
+  const progress = index / (totalItems - 1)
+
+  window.scrollTo({
+    top: scrollStart + progress * (scrollEnd - scrollStart),
+    behavior: "smooth",
+  })
+}
+
+const buttons = document.querySelectorAll('[data-index]');
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const index = Number(button.dataset.index)
+
+    scrollToHorizontalItem(index)
+  })
+})
