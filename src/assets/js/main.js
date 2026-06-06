f4543acc162fe6f384fbbad24b1d920758acc547
Codrin Pavel <zerospree@yahoo.com>
2026-06-06 16:55:59 +0100

    Remove lazy loading observer

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,7 @@
 import './modules/lenis.js';
 import './modules/scrollspy.js';
 import './modules/footer.js';
+import './modules/work.js';
 
 // animations
 import './modules/animate-wave.js';
---
+++ src/assets/js/modules/work.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const work = document.getElementById("work");
+
+    if (!work) return;
+
+    const makeImagesEager = () => {
+        work.querySelectorAll("img[loading='lazy']").forEach(img => {
+            img.loading = "eager";
+            img.setAttribute("loading", "eager");
+        });
+    };
+
+    const observer = new IntersectionObserver(
+        (entries) => {
+            if (entries[0].isIntersecting) {
+                makeImagesEager();
+                observer.disconnect(); // run only once
+            }
+        },
+        {
+            root: null,
+            rootMargin: "500px 0px 500px 0px",
+            threshold: 0
+        }
+    );
+
+    observer.observe(work);
+});
