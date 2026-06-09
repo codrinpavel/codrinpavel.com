be4f47d439391bf8750dcb3fc22ffc916b489083
Codrin Pavel <zerospree@yahoo.com>
2026-06-09 12:20:46 +0100

    Updates, quotes

--- src/data/accolades.js
+++ src/data/accolades.js
@@ -1,22 +1,34 @@
 export default function () {
   return [
     {
-      "content": "A pleasure to work with &mdash; personable and approachable, responsive and timely, optimistic and cares deeply about the quality of the end product.",
+      "content": "He possesses a unique combination of a remarkable technical aptitude and the ability to interpret and distill client feedback and apply it to achieve the desired goals. He cares deeply about the quality of the end product.",
       "name": "Ryan Bujeker",
       "title": "Digital Engagement Director",
       "company": "United"
     },
     {
-      "content": "Fantastic engineer to work with. He really understood the product from a customer point of view and worked hard to find the best solution to any problem.",
+      "content": "Fantastic engineer to work with. He really understood the product from a customer point of view and worked hard to find the best solution to any problem. I would love to work with Codrin again and would absolutely recommend him for any team.",
       "name": "Megan Berry",
       "title": "VP of Product",
       "company": "RebelMouse"
     },
     {
-      "content": "He is the rare developer that is fully client-facing and answers questions in a way that non-technical people can understand.",
+      "content": "Simply put: exceptional. He understands everything in context; he responds to everything. Codrin is the rare developer that is fully client-facing and answers questions in a way that non-technical people can understand.",
       "name": "Jade Harris",
       "title": "Senior Project Manager",
       "company": "Ideas On Purpose"
+    },
+    {
+      "content": "Codrin is skillful at finding solutions for both the designer and client needs. He leads by example and is caring, not only with the projects, but also the people who are in his team.",
+      "name": "James Kwak",
+      "title": "Senior Designer",
+      "company": "Ideas On Purpose"
+    },
+    {
+      "content": "Codrin took responsibility for a very hard piece and proved that he was able to manage all aspects of it, including talking to clients, reviewing code, managing his time and tasks, as well as those of his team.",
+      "name": "Nike Gurin-Petrovych",
+      "title": "CTO",
+      "company": "RebelMouse"
     }
   ]
 };
