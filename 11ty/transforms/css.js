0792b7db75063885c0b4acf3a72352708f7d6167
Codrin Pavel <zerospree@yahoo.com>
2026-06-16 14:23:23 +0100

    PurgeCss

--- 11ty/transforms/css.js
+++ 11ty/transforms/css.js
@@ -33,6 +33,7 @@ export default async function (content) {
           /^js/,
           /^is/,
           /^has/,
+          /^vt-/,
           /sr-only/,
           /animation/,
           /wave-word/,
