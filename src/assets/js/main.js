011aeea7a7ff70c29696011a7a3d0df9cda081f5
Codrin Pavel <zerospree@yahoo.com>
2026-06-17 14:19:29 +0100

    Updates

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,7 @@
 import './modules/lenis.js';
 import './modules/view-transition-types.js';
 import './modules/video-controls.js';
+import './modules/menu.js';
 
 // animations
 import './modules/inview.js';
---
+++ src/assets/js/modules/menu.js
@@ -0,0 +1,46 @@
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.menu');
+
+const BODY_CLASS = 'menu-open';
+const MENU_CLASS = 'is-open';
+
+// Initial state
+menu.inert = true;
+
+function openMenu() {
+  menu.classList.add(MENU_CLASS);
+  menu.inert = false;
+
+  menuButton.setAttribute('aria-expanded', 'true');
+  document.body.classList.add(BODY_CLASS);
+
+  const firstFocusable = menu.querySelector('a');
+  firstFocusable?.focus();
+}
+
+function closeMenu() {
+  menu.classList.remove(MENU_CLASS);
+  menu.inert = true;
+
+  menuButton.setAttribute('aria-expanded', 'false');
+  document.body.classList.remove(BODY_CLASS);
+
+  menuButton.focus();
+}
+
+function toggleMenu() {
+  menu.classList.contains(MENU_CLASS)
+    ? closeMenu()
+    : openMenu();
+}
+
+menuButton.addEventListener('click', toggleMenu);
+
+document.addEventListener('keydown', (event) => {
+  if (
+    event.key === 'Escape' &&
+    menu.classList.contains(MENU_CLASS)
+  ) {
+    closeMenu();
+  }
+});
