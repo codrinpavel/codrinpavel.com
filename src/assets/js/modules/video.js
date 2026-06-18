96ffd1fd7d599c2a0ee34a592249544bb97761b5
Codrin Pavel <zerospree@yahoo.com>
2026-06-19 00:28:01 +0100

    Updates

--- src/assets/js/modules/video.js
+++ src/assets/js/modules/video.js
@@ -1,23 +1,27 @@
-const video = document.querySelector(".reel");
+const videos = document.querySelectorAll(".reel");
 
-if (video) {
-  const observer = new IntersectionObserver(
-    ([entry]) => {
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const video = entry.target;
+
       if (entry.isIntersecting) {
         // Lazy-load only once
         if (!video.src) {
           video.src = video.dataset.src;
         }
 
-        video.play().catch(() => { });
+        video.play().catch(() => {});
       } else {
         video.pause();
       }
-    },
-    {
-      rootMargin: "200px 0px",
-    }
-  );
+    });
+  },
+  {
+    rootMargin: "200px 0px",
+  }
+);
 
+videos.forEach((video) => {
   observer.observe(video);
-}
+});
