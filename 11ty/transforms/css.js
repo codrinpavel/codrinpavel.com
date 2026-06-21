f833f9eb3aa10bf6568959dea7abab77ccf0ce29
Codrin Pavel <zerospree@yahoo.com>
2026-06-21 14:28:14 +0100

    Updates

--- 11ty/transforms/css.js
+++ 11ty/transforms/css.js
@@ -34,6 +34,8 @@ export default async function (content) {
           /^is-/,
           /^has-/,
           /^vt-/,
+          /--row/,
+          /--column/,
           /data-/,
           /sr-only/,
           /aria/,
