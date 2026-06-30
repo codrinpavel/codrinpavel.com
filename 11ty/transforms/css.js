23d4f2fe1f271eea1ce6db0d6a029bc93d096c63
Codrin Pavel <zerospree@yahoo.com>
2026-06-30 11:29:28 +0100

    Open menu

--- 11ty/transforms/css.js
+++ 11ty/transforms/css.js
@@ -34,6 +34,7 @@ export default async function (content) {
           /^is-/,
           /^has-/,
           /^vt-/,
+          /^menu-/,
           /lenis/,
           /data-/,
           /sr-only/,
